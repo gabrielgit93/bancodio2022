bb2d457f5ffb1d04eecb0c185f7fd56dbbfedfbc
gabrielgit93 <113723076+gabrielgit93@users.noreply.github.com>
2022-11-22 00:44:43 -0300

    Add files via upload

---
+++ ContaCorrente.java
@@ -0,0 +1,14 @@
+package desafiodioBanco;
+
+public class ContaCorrente extends Conta {
+      
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("===Extrato Conta Corrente===");
+		super.imprimirInfosComuns();
+
+	}
+
+	
+	
+}
---
+++ IConta.java
@@ -0,0 +1,13 @@
+package desafiodioBanco;
+
+public interface IConta {
+	
+    void sacar ( double valor);
+		
+    void depositar (double valor );
+		
+    void transferir (double valor, Conta contaDestino);
+	
+    void imprimirExtrato();
+    
+}
